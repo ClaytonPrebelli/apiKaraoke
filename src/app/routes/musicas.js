288618f64dd20f85ca99a719824aefd01b0a4f18
ClaytonPrebelli <clayton@prebelli.online>
2022-07-21 00:25:10 -0300

    inseridonovamente por categoria

--- src/app/routes/musicas.js
+++ src/app/routes/musicas.js
@@ -4,11 +4,12 @@ const mysql = require('../mysql').pool;
 
 
 //retorna todos os colaboradores
-router.get('/', (req, res, next) => {
+router.get('/:_cat', (req, res, next) => {
     mysql.getConnection((error, conn) => {
         if (error) { return res.status(500).send({ error: error }) };
         conn.query(
-            'SELECT * FROM musicas',
+            'SELECT * FROM musicas where categoria =?;',
+            [req.params._cat],
             (error, resultado, field) => {
                 if (error) { return res.status(500).send({ error: error }) };
                 return res.status(200).send(resultado)
